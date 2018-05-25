d750d13ec979fec8939d0bf0c0ced7c8a244d841
Degly Sebastián Pava Pava <dspavap@unal.edu.co>
2018-05-25 00:36:34 -0500

    Steam VR Training

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject moleContainer;
+	public TextMesh infoText;
+	public Hammer hammer;
+	public float spawnDuration = 1.5f;
+	public float spawnDecrement = 0.1f;
+	public float minimumSpawnDuration = 0.5f;
+	public float gameTimer = 15f;
+	public bool startedGame = false;
+
+	private Mole[] moles;
+	private float spawnTimer = 0f;
+	private float resetTimer = 5f;
+
+	// Use this for initialization
+	void Start () {
+		moles = moleContainer.GetComponentsInChildren<Mole> ();
+
+		moles[Random.Range(0, moles.Length)].Rise ();
+	}
+	
+	public void Initiate() {
+		startedGame = true;
+		resetTimer = 5f;
+		gameTimer = 15f;
+		hammer.score = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (startedGame == true)
+		{
+			gameTimer -= Time.deltaTime;
+			if (gameTimer > 0f)
+		{
+			infoText.text = "Interactions\nTime: " + Mathf.Floor(gameTimer) + "\nScore:" + hammer.score;
+			spawnTimer -= Time.deltaTime;
+		if (spawnTimer <= 0f)
+		{
+			moles[Random.Range(0, moles.Length)].Rise ();
+			spawnDuration -= spawnDecrement;
+			if (spawnDuration < minimumSpawnDuration)
+			{
+				spawnDuration = minimumSpawnDuration;
+			}
+			spawnTimer = spawnDuration;
+		}
+		} else
+		{
+			infoText.text = "Game over!";
+			resetTimer -= Time.deltaTime;
+			if (resetTimer <= 0f)
+			{
+				startedGame = false;
+				infoText.text = "Press Start!";
+			}
+		}
+		} else
+		{
+			infoText.text = "Pick Up \nHammer!";
+		}
+	}
+}
---
+++ Assets/Scripts/Hammer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hammer : MonoBehaviour {
+	public GameObject gameContainer;
+	public GameController gameOn;
+	private Vector3 initialPosition;
+	public int score = 0;
+
+	// Use this for initialization
+	void Awake () {
+		initialPosition = transform.localPosition;
+	}
+
+    void OnCollisionEnter (Collision col)
+    {
+        if(col.gameObject.tag.Equals("Mole") == true)
+        {
+         Mole mole = col.gameObject.GetComponent<Mole>();
+		 mole.OnHit ();
+		 score++;
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+			if (gameOn.startedGame == false)
+			{
+				transform.localPosition = initialPosition;
+			}
+			
+	}
+}
